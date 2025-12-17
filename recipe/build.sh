c8e201f21dc66e7545c4c32abff730a71694a367
Andre Anjos <andre.dos.anjos@gmail.com>
2025-12-17 10:48:21 +0100

    Add fixes proposed at https://github.com/ndonfris/fish-lsp/issues/130#issuecomment-3663712737

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,22 +14,14 @@ if [[ "${build_platform}" != "${target_platform}" ]]; then
     ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 fi
 
-# Fix package.json so it can bootstrap itself
-# Remove prepare script because it tries to call husky
-# Remove compile command from post install script so we don't try to transpile typescript again
-mv package.json package.json.bak
-jq 'del(.scripts.prepare)' package.json.bak > package.json
-sed -i 's/setup compile sh:relink/setup sh:relink/' package.json
-
 # Create package archive and install globally
-npm pack --ignore-scripts
+npm pack
 npm install -ddd \
     --global \
-    --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION//_/-}.tgz
 
 # Create license report for dependencies
-pnpm install --ignore-scripts
+pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 rm -rf ${PREFIX}/lib/node_modules/fish-lsp/node_modules/tree-sitter/build
