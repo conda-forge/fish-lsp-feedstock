28727ade66e57ca6dc7c45893b54aeba3fe01705
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-18 16:04:29 -0800

    make noarch

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,24 +2,24 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export PYTHON=${BUILD_PREFIX}/bin/python
+# Do not use tree-sitter, not really needed
+jq 'del(.dependencies["tree-sitter"])' package.json > tmp && mv tmp package.json
 
 # Create package archive and install globally
-case "${target_platform}" in
-  osx-arm64|linux-aarch64) export npm_config_arch="arm64" ;;
-esac
-
-if [[ "${build_platform}" != "${target_platform}" ]]; then
-    rm $PREFIX/bin/node
-    ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
-fi
-
-# Create package archive and install globally
-npm pack
-npm install -ddd --global ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION//_/-}.tgz
+npm pack --ignore-scripts
+npm install -ddd --global --no-bin-links --build-from-source ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION//_/-}.tgz
 
 # Create license report for dependencies
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-rm -rf ${PREFIX}/lib/node_modules/fish-lsp/node_modules/tree-sitter/build
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/fish-lsp <<EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/fish-lsp/dist/fish-lsp "\$@"
+EOF
+chmod +x ${PREFIX}/bin/fish-lsp
+
+tee ${PREFIX}/bin/fish-lsp.cmd <<EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\fish-lsp\dist\fish-lsp %*
+EOF
